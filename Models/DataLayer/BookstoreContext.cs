222f4c72ee8f21561724d5c7578dec29eaf2c96a
pavlitosdv <pavlos.papadopoulos88@gmail.com>
2021-05-16 22:57:57 +0300

    connectionstring added
    DbContext added and fluent API used to configure the many-to-many relashionship between Book and Author

---
+++ Models/DataLayer/BookstoreContext.cs
@@ -0,0 +1,48 @@
+﻿using Books.Models.DomainModels;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Books.Models.DataLayer
+{
+    public class BookstoreContext : DbContext
+    {
+        public BookstoreContext(DbContextOptions<BookstoreContext> options)
+          : base(options)
+        {
+        }
+
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<BookAuthor> BookAuthors { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {            
+            //set primary keys
+            modelBuilder.Entity<BookAuthor>().HasKey(ba => new { ba.BookId, ba.AuthorId });
+
+            //Many-to-many is broken down into two one-to-many relationship
+            //set foreign keys
+            modelBuilder.Entity<BookAuthor>().HasOne(ba => ba.Book)
+              .WithMany(b => b.BookAuthors)
+              .HasForeignKey(ba => ba.BookId);
+            modelBuilder.Entity<BookAuthor>().HasOne(ba => ba.Author)
+              .WithMany(b => b.BookAuthors)
+              .HasForeignKey(ba => ba.AuthorId);
+
+
+            // Cascading delete in genres set to be off. So, in order to delete a genre first it needs to delete 
+            //all the books that belong to that genre. With that we avoid to have orphan books that will not 
+            //belong to any genre
+            //remove cascading delete with genre
+            modelBuilder.Entity<Book>().HasOne(b => b.Genre)
+              .WithMany(g => g.Books)
+              .OnDelete(DeleteBehavior.Restrict);
+
+        }
+    }
+}
