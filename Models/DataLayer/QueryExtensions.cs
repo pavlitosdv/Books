d066a74c86c05844681eaf62b79681171ae264e9
pavlitosdv <pavlos.papadopoulos88@gmail.com>
2021-05-17 23:37:49 +0300

    Created generic extension method of IQueryable in order to be used for pagination

---
+++ Models/DataLayer/QueryExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Books.Models.DataLayer
+{
+    public static class QueryExtensions
+    {
+        public static IQueryable<T> PageBy<T>(this IQueryable<T> items, int pageNumber, int pageSize)
+        {
+            return items
+                    .Skip((pageNumber - 1) * pageSize)
+                    .Take(pageSize);
+        }
+    }
+}
