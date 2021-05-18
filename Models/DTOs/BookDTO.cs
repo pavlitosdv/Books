a63b71a705edfb0df39a57911ca9ecf0ba2f34e6
pavlitosdv <pavlos.papadopoulos88@gmail.com>
2021-05-18 23:43:03 +0300

    DTO classes created

---
+++ Models/DTOs/BookDTO.cs
@@ -0,0 +1,29 @@
+﻿using Books.Models.DomainModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Books.Models.DTOs
+{
+    public class BookDTO
+    {
+        public int BookId { get; set; }
+        public string Title { get; set; }
+        public double Price { get; set; }
+        public Dictionary<int, string> Authors { get; set; }
+
+        public void Load(Book book)
+        {
+            BookId = book.BookId;
+            Title = book.Title;
+            Price = book.Price;
+            Authors = new Dictionary<int, string>();
+
+            foreach (BookAuthor ba in book.BookAuthors)
+            {
+                Authors.Add(ba.AuthorId, ba.Author.FullName);
+            }
+        }
+    }
+}
