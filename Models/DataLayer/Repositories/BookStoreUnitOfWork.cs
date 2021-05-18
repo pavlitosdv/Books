a9b90703bb4db7f2f1a822a194e3f8a77af20f25
pavlitosdv <pavlos.papadopoulos88@gmail.com>
2021-05-18 23:23:20 +0300

    UnitOfWork implementation

---
+++ Models/DataLayer/Repositories/BookStoreUnitOfWork.cs
@@ -0,0 +1,85 @@
+﻿using Books.Models.DomainModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Books.Models.DataLayer.Repositories
+{
+    public class BookStoreUnitOfWork : IBookStoreUnitOfWork
+    {
+        private BookstoreContext context;
+        public BookStoreUnitOfWork(BookstoreContext ctx)
+        {
+            context = ctx;
+        }
+
+        private Repository<Book> bookData;
+        public Repository<Book> Books
+        {
+            get
+            {
+                if (bookData == null)
+                    bookData = new Repository<Book>(context);
+
+                return bookData;
+            }
+        }
+
+        private Repository<Author> authorData;
+        public Repository<Author> Authors
+        {
+            get
+            {
+                if (authorData == null)
+                    authorData = new Repository<Author>(context);
+
+                return authorData;
+            }
+        }
+
+        private Repository<BookAuthor> bookAuthorData;
+        public Repository<BookAuthor> BookAuthors
+        {
+            get
+            {
+                if (bookAuthorData == null)
+                    bookAuthorData = new Repository<BookAuthor>(context);
+
+                return bookAuthorData;
+            }
+        }
+
+        private Repository<Genre> genreData;
+        public Repository<Genre> Genres
+        {
+            get
+            {
+                if (genreData == null)
+                    genreData = new Repository<Genre>(context);
+
+                return genreData;
+            }
+        }
+
+        public void AddNewBookAuthors(Book book, int[] authorids)
+        {
+            book.BookAuthors = authorids.Select(i => new BookAuthor { Book = book, AuthorId = i }).ToList();
+        }
+
+        public void DeleteCurrentBookAuthors(Book book)
+        {
+            var currentAuthors = BookAuthors.List(new QueryOptions<BookAuthor>
+            {
+                Where = ba => ba.BookId == book.BookId
+            });
+
+            foreach (BookAuthor ba in currentAuthors)
+            {
+                BookAuthors.Delete(ba);
+            }
+        }
+
+        public void Save() => context.SaveChanges();
+    }
+}
---
+++ Models/DataLayer/Repositories/IBookStoreUnitOfWork.cs
@@ -0,0 +1,20 @@
+﻿using Books.Models.DomainModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Books.Models.DataLayer.Repositories
+{
+    public interface IBookStoreUnitOfWork
+    {
+        Repository<Book> Books { get; }
+        Repository<Author> Authors { get; }
+        Repository<BookAuthor> BookAuthors { get; }
+        Repository<Genre> Genres { get; }
+
+        void DeleteCurrentBookAuthors(Book book);
+        void AddNewBookAuthors(Book book, int[] authorids);
+        void Save();
+    }
+}
